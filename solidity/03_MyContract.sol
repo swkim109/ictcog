a448cf6cffc1239c6fd83e3c9c2234d4c33d4b60
swkim109 <swkim109@gmail.com>
2021-06-15 12:40:46 +0900

    .

--- solidity/03_MyContract.sol
+++ solidity/03_MyContract.sol
@@ -5,8 +5,8 @@ contract MyContract {
 
     function calc() public pure returns (uint8) {
 
-        uint8 x = 2**8-1;
-        uint8 b = x + 1;
+        uint8 x = 2**8-1; //256-1=255
+        uint8 b = x + 1; //255+1
         return b;
     }
 }
--- solidity/Calldata.sol
+++ solidity/Calldata.sol
@@ -6,4 +6,23 @@ contract MyContract {
     function makeCallData() public pure returns (bytes memory) {
         return abi.encodeWithSelector(bytes4(keccak256(bytes("set(string)"))), "Hello, World!");
     }
+
+    function f() public pure returns (bytes4) {
+        
+        return HelloWorld.set.selector;
+    }
+    
+    function g() public pure returns (bytes memory) {
+        return abi.encode("Hello, Crypto!");
+    }
+    
+    function h() public pure returns (bytes memory) {
+        return abi.encodeWithSelector(bytes4(keccak256(bytes("set(string)"))), "Hello, Crypto!");        
+    }
+}
+
+
+
+interface HelloWorld {
+    function set(string memory _s) external;
 }
