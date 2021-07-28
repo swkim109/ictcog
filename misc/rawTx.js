40b8a85e355b43b09f04b282964552b8670c6f53
swkim109 <swkim109@gmail.com>
2021-07-29 08:48:18 +0900

    .

--- misc/rawTx.js
+++ misc/rawTx.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 
-const privateKey = "409988a9e7e9097e5a42cb4b273e193958130f8e6a6cf7737307fc4ac5958899";
+const privateKey = "";
 const jsonRpcProvider = new ethers.providers.JsonRpcProvider("https://eth-rinkeby.alchemyapi.io/v2/n6XQhLyGF6RW6EQRdCEuQTkOvcrbS3ik");
 const fromAddr = "0x547d73355A851079E0395aDB2C647821b74C7eAF";
 const toAddr = "0xafc4f9f3ba806dd2f8e47a524ffda2418bbfc08a";
