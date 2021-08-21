d9050dfcfcc26e9f39f7c1da3e609b273af15472
swkim109 <swkim109@gmail.com>
2021-08-21 12:34:44 +0900

    .

--- misc/rawTx_web3.js
+++ misc/rawTx_web3.js
@@ -1,7 +1,7 @@
 const Accounts = require('web3-eth-accounts');
 const accounts = new Accounts('https://eth-rinkeby.alchemyapi.io/v2/n6XQhLyGF6RW6EQRdCEuQTkOvcrbS3ik');
 
-const privateKey = "409988a9e7e9097e5a42cb4b273e193958130f8e6a6cf7737307fc4ac5958899";
+const privateKey = "";
 const n = "0x33";
 const fromAddr = "0x547d73355A851079E0395aDB2C647821b74C7eAF";
 const toAddr = "0xAd36301E8C66bB2Af80c63DA5a99BdF2c202c9a1";
